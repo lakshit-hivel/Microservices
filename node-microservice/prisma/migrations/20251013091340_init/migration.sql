054cbc7f49d38e0b47e5d772ca234f7fa9716a40
lakshit-hivel <lakshit@hivel.ai>
2025-10-13 15:30:31 +0530

    Update project configuration: enhance .gitignore to exclude environment files and generated Prisma files; update package.json with new scripts, dependencies, and devDependencies for improved development workflow.

---
+++ node-microservice/prisma/migrations/20251013091340_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "country" TEXT NOT NULL DEFAULT 'India',
+    "gender" TEXT NOT NULL,
+    "profilePicture" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'user',
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
