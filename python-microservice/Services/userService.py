6650a385bafcef3ae4c57cd19d692482fb223d62
lakshit-hivel <lakshit@hivel.ai>
2025-10-16 17:59:18 +0530

    implemented user retrieval by ID, listing deleted users, restoring deleted users, and soft deletion of users.

--- python-microservice/Services/userService.py
+++ python-microservice/Services/userService.py
@@ -12,3 +12,23 @@ def create_user(db:Session, user:UserCreate):
 
 def get_all_users(db:Session):
     return db.query(User).all()
+
+def get_user_by_id(db:Session, user_id:int):
+    return db.query(User).filter(User.id == user_id).first()
+
+def get_deleted_users(db:Session):
+    return db.query(User). filter(User.isDeleted==True).all()
+
+def restore_user(db:Session, user_id:int):
+    user = db.query(User).filter(User.id == user_id).first()
+    user.isDeleted = False
+    db.commit()
+    db.refresh(user)
+    return user
+
+def delete_user(db:Session, user_id:int):
+    user = db.query(User).filter(User.id == user_id).first()
+    user.isDeleted = True
+    db.commit()
+    db.refresh(user)
+    return user
--- python-microservice/router/userRouter.py
+++ python-microservice/router/userRouter.py
@@ -7,12 +7,34 @@ from database import get_db, engine
 
 router = APIRouter(prefix="/api")
 
-@router.get("/users", response_model=list[schemas.User])
+@router.get("/all-users", response_model=list[schemas.User])
 def get_all_users(db: Session = Depends(get_db)):
     users = userService.get_all_users(db)
     return users
 
-@router.post("/users", response_model=schemas.User)
+@router.post("/new-user", response_model=schemas.User)
 def create_new_user(user:schemas.UserCreate, db: Session = Depends(get_db)):
     new_user = userService.create_user(db, user)
     return new_user
+
+@router.get("/get-user/{user_id}", response_model=schemas.User)
+def get_user_by_id(user_id:int, db: Session = Depends(get_db)):
+    user = userService.get_user_by_id(db, user_id=user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+@router.get("/all-deleted-users", response_model=list[schemas.User])
+def get_deleted_users(db: Session = Depends(get_db)):
+    deleted_users = userService.get_deleted_users(db)
+    return deleted_users
+
+@router.put("/restore-user/{user_id}", response_model=schemas.User)
+def restore_deleted_user(user_id:int, db: Session = Depends(get_db)):
+    restore_user = userService.restore_user(db, user_id)
+    return restore_user
+
+@router.delete("/delete-user/{user_id}", response_model=schemas.User)
+def delete_user(user_id:int, db: Session = Depends(get_db)):
+    deleted_user = userService.delete_user(db, user_id)
+    return deleted_user
