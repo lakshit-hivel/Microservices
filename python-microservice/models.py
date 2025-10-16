c6380fd2d82e25c67e0afc5fcb649ecc2b4c2362
lakshit-hivel <lakshit@hivel.ai>
2025-10-16 15:30:55 +0530

    FastAPI setup with SQLAlchemy and schema validation

---
+++ python-microservice/models.py
@@ -0,0 +1,38 @@
+# models.py
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, func
+from .database import Base
+
+class User(Base):
+    __tablename__ = "User"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False, index=True)
+    age = Column(Integer, nullable=False)
+    university = Column(String, nullable=False)
+    company = Column(String, nullable=False)
+    title = Column(String, nullable=False)
+    department = Column(String, nullable=False)
+    address = Column(String, nullable=False)
+    state = Column(String, nullable=False)
+    city = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
+    country = Column(String, nullable=False, default="India")
+    gender = Column(String, nullable=False)
+    profilePicture = Column(String, nullable=False)
+    role = Column(String, nullable=False, default="user")
+    isDeleted = Column(Boolean, nullable=False, default=False)
+    deletedAt = Column(DateTime, nullable=True)
+    createdAt = Column(DateTime(timezone=True), server_default=func.now())
+    updatedAt = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now())
+
+
+class AuthUser(Base):
+    __tablename__ = "authUser"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, nullable=False)
+    username = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    createdAt = Column(DateTime(timezone=True), server_default=func.now())
+    updatedAt = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now())
---
+++ python-microservice/schemas.py
@@ -0,0 +1,54 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class UserBase(BaseModel):
+    name: str
+    email: str
+    age: int
+    university: str
+    company: str
+    title: str
+    department: str
+    address: str
+    state: str
+    city: str
+    phone: str
+    country: str = "India"
+    gender: str
+    profilePicture: str
+    role: str = "user"
+    isDeleted: bool = False
+
+class UserCreate(UserBase):
+    pass
+
+class UserDelete(BaseModel):
+    id: int
+    isDeleted: bool = True
+
+class UserGet(UserBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+
+class AuthUserBase(BaseModel):
+    email: str
+    username: str
+
+class AuthUserCreate(AuthUserBase):
+    password: str
+
+class AuthUserLogin(BaseModel):
+    username: str
+    password: str
+
+class AuthUserGet(AuthUserBase):
+    id: int
+    createdAt: datetime
+    updatedAt: datetime
+
+    class Config:
+        from_attributes = True
