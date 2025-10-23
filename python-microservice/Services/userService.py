adb0795631dd959bcf44c9c84c15d7872d8ca710
lakshit-hivel <lakshit@hivel.ai>
2025-10-23 15:57:17 +0530

    Filter out deleted users in get_all_users function

--- python-microservice/Services/userService.py
+++ python-microservice/Services/userService.py
@@ -11,7 +11,7 @@ def create_user(db:Session, user:UserCreate):
     return new_user_instance
 
 def get_all_users(db:Session):
-    return db.query(User).all()
+    return db.query(User).filter(User.isDeleted==False).all()
 
 def get_user_by_id(db:Session, user_id:int):
     return db.query(User).filter(User.id == user_id).first()
